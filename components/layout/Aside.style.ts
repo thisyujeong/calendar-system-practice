a940b9529051759c170fc5b5e0407dd16eb51a6d
thisyujeong <this.yujeong@gmail.com>
2022-06-25 22:21:52 +0900

    feat: aside toggle styles

--- components/layout/Aside.style.ts
+++ components/layout/Aside.style.ts
@@ -1,11 +1,33 @@
 import styled from '@emotion/styled';
 import { Common } from '../../styles/common';
+import { css } from '@emotion/react';
 
-export const AsideContainer = styled.section`
-  border-right: 2px solid #2c3e5010;
+export const AsideContainer = styled.section<{ open: boolean }>`
   position: relative;
+  min-width: ${({ open }) => (open ? '260px' : '0')};
+  border-right: 2px solid #2c3e5010;
+  transition: min-width 0.2s ease;
 `;
 
+export const AsideInner = styled.div<{ open: boolean }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: ${({ open }) => (open ? 1 : 0)};
+  transition: opacity 0.2s ease;
+`;
+
+const dynamicToggle = ({ open }: { open: boolean }) => {
+  if (!open) {
+    return css`
+      right: -12px;
+      width: 30px;
+      border-radius: 0;
+      border-top-right-radius: 5px;
+      border-bottom-right-radius: 5px;
+    `;
+  }
+};
 export const AsideToggle = styled.div<{ open: boolean }>`
   position: absolute;
   top: 58px;
@@ -20,6 +42,8 @@ export const AsideToggle = styled.div<{ open: boolean }>`
   z-index: 10;
   cursor: pointer;
 
+  ${dynamicToggle}
+
   &:after {
     content: '';
     position: absolute;
@@ -32,13 +56,13 @@ export const AsideToggle = styled.div<{ open: boolean }>`
     transition: border 0.2s ease;
     transform: ${({ open }) =>
       open
-        ? 'translate(-75%, -50%) rotate(-45deg)'
-        : 'translate(-25%, -50%) rotate(135deg)'};
+        ? 'translate(-25%, -50%) rotate(135deg)'
+        : 'translate(-75%, -50%) rotate(-45deg)'};
   }
 
   &:hover,
   &:hover:after {
-    border-color: #2c3e5080;
+    border-color: #29b48b;
   }
 `;
 
--- components/layout/Container.style.ts
+++ components/layout/Container.style.ts
@@ -2,15 +2,16 @@ import styled from '@emotion/styled';
 import { Common } from '../../styles/common';
 
 export const Container = styled.div`
-  display: grid;
+  display: flex;
   position: relative;
-  grid-template-columns: 260px 1fr;
+  /* grid-template-columns: 260px 1fr; */
   height: 100vh;
 `;
 
 export const Content = styled.div`
   position: relative;
-  grid-template-rows: ${Common.layout.headerHeight} 1fr;
+  width: 100%;
+  /* grid-template-rows: ${Common.layout.headerHeight} 1fr; */
 `;
 
 export const Main = styled.main`
