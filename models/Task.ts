a4793aa66da2e83ae7cb78c60a3f942b3faae1e0
thisyujeong <this.yujeong@gmail.com>
2022-06-30 23:49:17 +0900

    chore: 오타 수정

--- models/Task.ts
+++ models/Task.ts
@@ -19,4 +19,4 @@ const TaskSchema = new mongoose.Schema({
   ],
 });
 
-export default mongoose.models.User || mongoose.model('User', TaskSchema);
+export default mongoose.models.Task || mongoose.model('Task', TaskSchema);
