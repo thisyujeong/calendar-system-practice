1f5f758b382ca5a19dd1b44a12470a980b864e21
thisyujeong <this.yujeong@gmail.com>
2022-07-04 01:28:16 +0900

    feat: Task Axios 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,26 @@
-import type { NextPage } from 'next';
-import React from 'react';
+import { useEffect } from 'react';
+import { getSession, useSession } from 'next-auth/react';
 import LayoutContianer from '../components/layout/Container';
 import Weekly from '../components/weekly/Weekly';
+import axios from 'axios';
+import type { InferGetStaticPropsType } from 'next';
 
-const Home: NextPage = () => {
+const Home = ({ tasks }: InferGetStaticPropsType<typeof getServerSideProps>) => {
+  console.log('tasks', tasks);
+  useEffect(() => {}, []);
   return (
     <LayoutContianer>
       <Weekly />
     </LayoutContianer>
   );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get('http://localhost:3000/api/tasks');
+  const tasks = data.data;
+  return {
+    props: { tasks },
+  };
 };
 
 export default Home;
